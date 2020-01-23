65d7af1fa485cf0cc2426392d4b92a96c4d83610
chrissteinsland <chrissteinsland@users.noreply.github.com>
2020-01-23 11:46:02 +0100

    Co-authored-by: Daniel Rahme <daniel.rahme.dr@gmail.com>

--- Part2/python/foo.py
+++ Part2/python/foo.py
@@ -1,21 +1,27 @@
 # Python 3.3.3 and 2.7.6
 # python fo.py
 
-from threading import Thread
+from threading import Thread, Lock
 
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
 i = 0
-
+mutex = Lock()
 def incrementingFunction():
     global i
-    # TODO: increment i 1_000_000 times
+    for j in range(0,1000000):
+        mutex.acquire()
+        i+=1
+        mutex.release()
+
 
 def decrementingFunction():
     global i
-    # TODO: decrement i 1_000_000 times
-
+    for j in range(0,999000):
+        mutex.acquire()
+        i-=1
+        mutex.release()
 
 
 def main():
@@ -24,8 +30,9 @@ def main():
     incrementing = Thread(target = incrementingFunction, args = (),)
     decrementing = Thread(target = decrementingFunction, args = (),)
     
-    # TODO: Start both threads
-    
+    incrementing.start()
+    decrementing.start()
+
     incrementing.join()
     decrementing.join()
     
